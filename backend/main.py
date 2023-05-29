e8daf654b7dc900b67471ccb6f08c7eace41691f
karolskiba <skiba.kar@gmail.com>
2023-05-29 13:16:55 +0200

    routing

--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,6 @@ from var import VAR
 from connection import Connection
 
 
-
-
 app = FastAPI()
 origins = ["*"]
 app.add_middleware(
@@ -22,6 +20,7 @@ db = Connection()
 connection = db.connection
 engine = db.engine
 
+
 @app.get("/")
 async def root():
 
@@ -29,21 +28,24 @@ async def root():
 
 # my_url(my_id: int = Query(...), my_type: int = Query(None)):
 
-@app.get("/stock_data/")
+
+@app.get("/stock_data")
 async def get_stock(ticker_code, start_date: str, end_date: str):
     print("getting from db...")
-    df = db.get_stock_data(ticker_code, connection=connection)
-    if not df:
+    response = db.get_stock_data(
+        ticker_code, connection=connection, start=start_date, end=end_date)
+    if not response:
         response = db.get_unknown_stock(ticker=ticker_code)
         db.insert_stock_data(response['raw_df'], engine=engine)
         return response['data']
     else:
-        return df
+        return response['data']
+
 
 @app.get("/var/{ticker_code}")
-async def get_var(ticker_code:str, start_date: str, end_date: str):
+async def get_var(ticker_code: str, start_date: str, end_date: str):
     model = VAR(
-         ticker_code,
+        ticker_code,
         start_date,
         end_date,
         0.95,
@@ -53,4 +55,3 @@ async def get_var(ticker_code:str, start_date: str, end_date: str):
     return model.var
 # @app.get("/list_of_stocks")
 # async def list_of_stocks():
-    
--- stock-app/src/App.tsx
+++ stock-app/src/App.tsx
@@ -1,20 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  const [counter, setCounter] = useState<number>(0);
-
   return (
-    <div>
-      <button
-        onClick={() => {
-          setCounter((prev) => prev + 1);
-        }}
-      >
-        Click Me
-      </button>
-      {counter}
-    </div>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="*" element={<MainPage />} />
+    </Routes>
   );
 }
 
